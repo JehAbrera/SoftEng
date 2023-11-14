2bf3a374c569210f168ba633059008693e88e57d
JehAbrera <95663191+JehAbrera@users.noreply.github.com>
2023-11-14 19:26:58 +0800

    Add files via upload

---
+++ ReuseContent.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+$_SESSION['isLoggedIn'] = true;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reuse.css">
+    <!-- Font Awesome Icon Script -->
+    <script src="https://kit.fontawesome.com/678a3c402d.js" crossorigin="anonymous"></script>
+    <title>Reuse Nav Footer</title>
+</head>
+
+<body>
+    <div class="content-wrapper">
+        <!-- Reusable Nav -->
+        <nav class="nav-wrapper">
+            <div class="navicon-wrapper">
+                <div class="icon-container">
+                    <i class="fa-solid fa-church"></i> SJCP
+                </div>
+            </div>
+            <div class="nav-content">
+                <div class="nav-item">Home</div>
+                <div class="nav-item">FAQs</div>
+                <div class="nav-item dropdown">
+                    <span class="dp-title">Services <i class="fa-solid fa-angle-down"></i></span>
+                    <div class="dropdown-content">
+                        <div class="nav-item">View Services</div>
+                        <div class="nav-item">Set Appointment</div>
+                        <div class="nav-item">View Appointment</div>
+                        <div class="nav-item">Search Record</div>
+                    </div>
+                </div>
+                <div class="nav-item">Events</div>
+                <div class="nav-item">About Us</div>
+                <?php
+                if (!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] == false) {
+                    echo '<div class="nav-item">Login</div>';
+                } else if ($_SESSION['isLoggedIn'] == true) {
+                    echo '<div class="nav-item dropdown">' ?>
+                    <span class="dp-title">Profile <i class="fa-solid fa-angle-down"></i></span>
+                    <div class="dropdown-content">
+                        <div class="nav-item">Profile</div>
+                        <div class="nav-item">Log-Out</div>
+                    </div>
+                <?php '</div>';
+                }
+                ?>
+            </div>
+        </nav>
+        <!-- Filler Body - No need to Copy -->
+        <div class="main-body-wrapper">
+            <section class="main-content"></section>
+        </div>
+        <!-- Reusable Footer -->
+        <footer class="footer-wrapper">
+
+        </footer>
+    </div>
+    <script src="reuse.js"></script>
+</body>
+
+</html>
---
+++ reuse.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.nav-wrapper');
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('nav-scrolled');
+    } else {
+        navbar.classList.remove('nav-scrolled');
+    }
+};
