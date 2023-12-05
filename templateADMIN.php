be46797a6fe7087986499a8863d8640e638b3b29
JehAbrera <95663191+JehAbrera@users.noreply.github.com>
2023-12-05 23:19:43 +0800

    Admin Side Nav Template

---
+++ templateADMIN.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleTEMPLATEADMIN.css">
+    <!-- Font Awesome Icon Script -->
+    <script src="https://kit.fontawesome.com/678a3c402d.js" crossorigin="anonymous"></script>
+    <title>The SJCP</title>
+</head>
+<body>
+    <div class="content-wrapper">
+        <!-- Nav Wrapper -->
+        <div class="nav-wrapper">
+            <div class="icon-wrapper">
+                <i class="fa-solid fa-church"></i> SJCP
+            </div>
+            <div class="nav-items">
+                <button class="active-btn">Dashboard</button>
+                <button>Add Announcement</button>
+                <button>Records</button>
+                <button>Appointments</button>
+                <button>Log-out</button>
+            </div>
+        </div>
+
+        <!-- Main Content Wrapper -->
+        <div class="main-content">
+
+        </div>
+    </div>
+</body>
+</html>
