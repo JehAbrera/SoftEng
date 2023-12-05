7656a956c6cfca4a15fab1d403fb9af5529a6803
J <97033015+JephHodreal@users.noreply.github.com>
2023-12-05 11:09:02 +0800

    Add files via upload

--- db_church.sql
+++ db_church.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2023 at 09:29 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Dec 05, 2023 at 04:07 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,14 +45,27 @@ CREATE TABLE `appointment_details` (
   `appointment_id` int(40) NOT NULL,
   `name` varchar(120) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `date_appointed` datetime NOT NULL DEFAULT curdate(),
-  `time_appointed` datetime NOT NULL DEFAULT curtime(),
-  `event_date` datetime NOT NULL DEFAULT curdate(),
-  `event_time` datetime NOT NULL DEFAULT curtime(),
+  `date_appointed` date NOT NULL,
+  `time_appointed` time NOT NULL,
+  `event_date` date NOT NULL,
+  `event_timeStart` time NOT NULL,
+  `event_timeEnd` time NOT NULL,
   `appointment_type` varchar(30) NOT NULL,
   `appointment_status` varchar(15) NOT NULL,
   `reason` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment_details`
+--
+
+INSERT INTO `appointment_details` (`appointment_id`, `name`, `email`, `date_appointed`, `time_appointed`, `event_date`, `event_timeStart`, `event_timeEnd`, `appointment_type`, `appointment_status`, `reason`) VALUES
+(1, 'John Doe', 'johndoe@gmail.com', '2023-11-30', '10:00:00', '2023-12-07', '10:00:00', '10:45:00', 'Baptism', 'Accepted', 'You are accepted.'),
+(2, 'Jane Girl', 'janegirl@gmail.com', '2023-12-01', '11:00:00', '2023-12-07', '14:00:00', '15:15:00', 'Wedding', 'Accepted', 'Dun dun dunun'),
+(3, 'jane the Virgin', 'janethevirgin@yahoo.com', '2023-12-01', '11:00:00', '2023-12-08', '08:00:00', '00:00:00', 'Funeral Mass/Blessing', 'Accepted', 'namatay ih'),
+(5, 'sample', 'sample@yahoo.com', '0000-00-00', '00:00:00', '2023-12-28', '00:00:00', '00:00:00', 'Blessing', 'Pending', ''),
+(6, 'sample', 'sample@yahoo.com', '0000-00-00', '00:00:00', '2023-12-28', '00:00:00', '00:00:00', 'Blessing', 'Pending', ''),
+(7, 'sample', 'sample@yahoo.com', '0000-00-00', '00:00:00', '2023-12-28', '00:00:00', '00:00:00', 'Blessing', 'Pending', '');
 
 -- --------------------------------------------------------
 
@@ -62,13 +75,14 @@ CREATE TABLE `appointment_details` (
 
 CREATE TABLE `baptism_details` (
   `appointment_id` int(40) NOT NULL,
-  `event_date` datetime NOT NULL DEFAULT curdate(),
-  `event_time` datetime NOT NULL DEFAULT curtime(),
+  `event_date` date NOT NULL,
+  `event_timeStart` time NOT NULL,
+  `event_timeEnd` time NOT NULL,
   `lastName` varchar(40) NOT NULL,
   `firstName` varchar(40) NOT NULL,
   `middleName` varchar(40) NOT NULL,
   `gender` varchar(6) NOT NULL,
-  `dob` datetime NOT NULL,
+  `dob` date NOT NULL,
   `pob` varchar(120) NOT NULL,
   `address` varchar(120) NOT NULL,
   `contactNum` varchar(13) NOT NULL,
@@ -86,13 +100,35 @@ CREATE TABLE `baptism_details` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blessing_details`
+--
+
+CREATE TABLE `blessing_details` (
+  `appointment_id` int(11) NOT NULL,
+  `contactNum` varchar(13) NOT NULL,
+  `event_date` date NOT NULL,
+  `event_time` time NOT NULL,
+  `blessing_type` varchar(20) NOT NULL,
+  `address` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blessing_details`
+--
+
+INSERT INTO `blessing_details` (`appointment_id`, `contactNum`, `event_date`, `event_time`, `blessing_type`, `address`) VALUES
+(1, '3456789', '2023-12-28', '00:00:00', 'House Blessing', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `document_request_details`
 --
 
 CREATE TABLE `document_request_details` (
   `appointment_id` int(40) NOT NULL,
-  `claim_date` datetime NOT NULL DEFAULT curdate(),
-  `claim_time` datetime NOT NULL DEFAULT curtime(),
+  `claim_date` date NOT NULL,
+  `claim_time` time NOT NULL,
   `documentType` varchar(50) NOT NULL,
   `firstName` varchar(40) NOT NULL,
   `middleName` varchar(40) NOT NULL,
@@ -110,13 +146,15 @@ CREATE TABLE `document_request_details` (
 
 CREATE TABLE `funeral_details` (
   `appointment_id` int(40) NOT NULL,
-  `event_date` datetime NOT NULL DEFAULT curdate(),
-  `event_time` datetime NOT NULL DEFAULT curtime(),
+  `event_date` date NOT NULL,
+  `event_timeStart` time NOT NULL,
+  `event_timeEnd` time NOT NULL,
   `lastName` varchar(40) NOT NULL,
   `firstName` varchar(40) NOT NULL,
   `middleName` varchar(40) NOT NULL,
   `gender` varchar(6) NOT NULL,
-  `date_of_death` datetime NOT NULL,
+  `date_of_death` date NOT NULL,
+  `age` int(11) NOT NULL,
   `cause_of_death` varchar(120) NOT NULL,
   `date_of_interment` datetime NOT NULL,
   `place_of_cemetery` varchar(120) NOT NULL,
@@ -141,6 +179,14 @@ CREATE TABLE `login_userinfo` (
   `user_password` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `login_userinfo`
+--
+
+INSERT INTO `login_userinfo` (`firstName`, `lastName`, `email`, `contactNum`, `user_password`) VALUES
+('jelika', 'palad', 'jelikapalad@gmail.com', '09123456789', 'jelikapalad'),
+('john rey', 'abrera', 'test@gmail.com', '09193456789', 'a51dda7c7ff50b61eaea0444371f4a6a9301e501');
+
 -- --------------------------------------------------------
 
 --
@@ -150,8 +196,8 @@ CREATE TABLE `login_userinfo` (
 CREATE TABLE `mass_intention_details` (
   `appointment_id` int(40) NOT NULL,
   `contactNum` varchar(13) NOT NULL,
-  `event_date` datetime NOT NULL DEFAULT curdate(),
-  `event_time` datetime NOT NULL DEFAULT curtime(),
+  `event_date` date NOT NULL,
+  `event_time` time NOT NULL,
   `purpose` varchar(120) NOT NULL,
   `name` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -272,13 +318,14 @@ CREATE TABLE `record_wedding_details` (
 
 CREATE TABLE `wedding_details` (
   `appointment_id` int(40) NOT NULL,
-  `event_date` datetime NOT NULL DEFAULT curdate(),
-  `event_time` datetime NOT NULL DEFAULT curtime(),
+  `event_date` date NOT NULL,
+  `event_timeStart` time NOT NULL,
+  `event_timeEnd` time NOT NULL,
   `groom_lastName` varchar(40) NOT NULL,
   `groom_firstName` varchar(40) NOT NULL,
   `groom_middleName` varchar(40) NOT NULL,
   `groom_contactNum` varchar(13) NOT NULL,
-  `groom_dob` datetime NOT NULL,
+  `groom_dob` date NOT NULL,
   `groom_pob` varchar(120) NOT NULL,
   `groom_address` varchar(120) NOT NULL,
   `groom_fatherName` varchar(100) NOT NULL,
@@ -288,7 +335,7 @@ CREATE TABLE `wedding_details` (
   `bride_firstName` varchar(40) NOT NULL,
   `bride_middleName` varchar(40) NOT NULL,
   `bride_contactNum` varchar(13) NOT NULL,
-  `bride_dob` datetime NOT NULL,
+  `bride_dob` date NOT NULL,
   `bride_pob` varchar(120) NOT NULL,
   `bride_address` varchar(120) NOT NULL,
   `bride_fatherName` varchar(100) NOT NULL,
@@ -325,6 +372,12 @@ ALTER TABLE `funeral_details`
   ADD KEY `appointment_id` (`appointment_id`);
 
 --
+-- Indexes for table `login_userinfo`
+--
+ALTER TABLE `login_userinfo`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- Indexes for table `mass_intention_details`
 --
 ALTER TABLE `mass_intention_details`
@@ -344,7 +397,7 @@ ALTER TABLE `wedding_details`
 -- AUTO_INCREMENT for table `appointment_details`
 --
 ALTER TABLE `appointment_details`
-  MODIFY `appointment_id` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `appointment_id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `baptism_details`
